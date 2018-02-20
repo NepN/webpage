10eebcde583ec1295802100a819383dc178b7b2e
Léon <leon.geide@mail.de>
2018-02-20 18:23:55 +0100

    tests

--- home.php
+++ home.php
@@ -1,7 +1,21 @@
-<?php
-  $fp = fopen("content.txt","r");
-  if ($fi = file("datei.txt"))
-    foreach($fi as $ftemp => $row) echo "Zeile $ftemp: $row<br>";
-  else echo "Datei nicht gefunden";
-  fclose($fp);
-?>
+<html>
+
+  <head>
+  
+    <title> test </title>
+  
+  </head>
+  
+  <body>
+  
+    <?php
+      $fp = fopen("content.txt","r"
+      if ($fi = file("datei.txt"))
+        foreach($fi as $ftemp => $row) echo "Zeile $ftemp: $row<br>";
+      else echo "Datei nicht gefunden";
+    ?>
+  
+  </body>
+
+</html>
+
