fd8977a952d0b5a7b0e924cda7e33c73257ad95d
Léon <leon.geide@mail.de>
2018-02-20 18:41:48 +0100

    t

--- home.php
+++ home.php
@@ -10,8 +10,9 @@
   
     <?php
       $fp = fopen("content.txt","r");
-      if ($fi = file("datei.txt"))
-        foreach($fi as $ftemp => $row) 
+      if ( $fi = file("datei.txt") )
+      {
+        foreach ( $fi as $ftemp => $row ) 
         {
           echo "Zeile $ftemp: $row<br>";
         }
